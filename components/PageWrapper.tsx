38ad2403aee6facbd7f6fc9dc5a671b8cd6112d2
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2020-05-15 19:01:16 +0200

    Add Google Analytics

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,20 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { loadGoogleAnalytics } from '../utils/loadGoogleAnalytics';
 import { GlobalStyles } from './GlobalStyles';
 import { Header } from './Header';
 
-export const PageWrapper: FC = ({ children }) => (
-  <>
-    <div className="root">
-      <GlobalStyles />
-      <Header />
-      {children}
-    </div>
-    <style jsx>{`
-      .root {
-        max-width: 35em;
-        margin: auto;
-        padding: 10px 15px;
-      }
-    `}</style>
-  </>
-);
+export const PageWrapper: FC = ({ children }) => {
+  useEffect(() => {
+    loadGoogleAnalytics();
+  }, []);
+
+  return (
+    <>
+      <div className="root">
+        <GlobalStyles />
+        <Header />
+        {children}
+      </div>
+      <style jsx>{`
+        .root {
+          max-width: 35em;
+          margin: auto;
+          padding: 10px 15px;
+        }
+      `}</style>
+    </>
+  );
+};
