45a8ad36ce6900161848287772d4dd9ad5de6c8f
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2022-12-05 14:41:36 +0100

    Add AoC 2022 and organize projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -10,30 +10,25 @@ const Index = () => (
     </Head>
     <div>
       <h1>Projects</h1>
+      <h2>Apps</h2>
       <ul>
         <li className="listItem">
           <ExternalLink href="https://pakt.me/">
             <h2 className="itemTitle">Pakt</h2>
           </ExternalLink>
         </li>
+
         <li className="listItem">
-          <ExternalLink href="https://github.com/alexbensimon/aoc-2021">
-            <h2 className="itemTitle">AoC 2021 in Elixir</h2>
+          <ExternalLink href="https://github.com/alexbensimon/qure">
+            <h2 className="itemTitle">Qure</h2>
           </ExternalLink>
         </li>
+      </ul>
+      <h2>Websites</h2>
+      <ul>
         <li className="listItem">
           <ExternalLink href="https://github.com/alexbensimon/laccord-parfait">
             <h2 className="itemTitle">L'accord parfait</h2>
-          </ExternalLink>
-        </li>
-        <li className="listItem">
-          <ExternalLink href="https://replit.com/@alexbensimon/DiscorDynamic">
-            <h2 className="itemTitle">Discord bot on Replit</h2>
-          </ExternalLink>
-        </li>
-        <li className="listItem">
-          <ExternalLink href="https://github.com/alexbensimon/advent-of-code-2020">
-            <h2 className="itemTitle">AoC 2020 in Rust</h2>
           </ExternalLink>
         </li>
         <li className="listItem">
@@ -46,9 +41,30 @@ const Index = () => (
             <h2 className="itemTitle">Mom's website</h2>
           </ExternalLink>
         </li>
+      </ul>
+      <h2>Advent of Code</h2>
+      <ul>
         <li className="listItem">
-          <ExternalLink href="https://github.com/alexbensimon/qure">
-            <h2 className="itemTitle">Qure</h2>
+          <ExternalLink href="https://github.com/alexbensimon/aoc-2022">
+            <h2 className="itemTitle">AoC 2022 in Python</h2>
+          </ExternalLink>
+        </li>
+        <li className="listItem">
+          <ExternalLink href="https://github.com/alexbensimon/aoc-2021">
+            <h2 className="itemTitle">AoC 2021 in Elixir</h2>
+          </ExternalLink>
+        </li>
+        <li className="listItem">
+          <ExternalLink href="https://github.com/alexbensimon/advent-of-code-2020">
+            <h2 className="itemTitle">AoC 2020 in Rust</h2>
+          </ExternalLink>
+        </li>
+      </ul>
+      <h2>Misc</h2>
+      <ul>
+        <li className="listItem">
+          <ExternalLink href="https://replit.com/@alexbensimon/DiscorDynamic">
+            <h2 className="itemTitle">Discord bot on Replit</h2>
           </ExternalLink>
         </li>
       </ul>
